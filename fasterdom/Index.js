7e1427c553f621fb4ebdeafa1b881648b4102d8a
Lemon <165233560+BludIsAnLemon@users.noreply.github.com>
2024-11-02 11:46:00 +0800

    Create Index.js

---
+++ fasterdom/Index.js
@@ -0,0 +1,6 @@
+const q = (select) => {return document.querySelector(select)};
+const qa = (select) => {return document.querySelectorAll(select)};
+const eid = (elemtId) => {return document.getElementById(elemId)};
+const etn = (elemTag) => {return document.getElementsByTagName(elemTag)};
+const ecn = (elemClass) => {return document.getElementsByClassName(elemClass)};
+const eyn = (elemName) => {return document.getElementsByName(elemName)};
